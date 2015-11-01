45e5ba57250a2c699566854430473803af018d0c
Robin Spinks <uglybluecat@googlemail.com>
2015-11-01 12:28:18 +0000

    Initial Commit

---
+++ AppyParkingTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppyParkingTest
+//
+//  Created by Robin Spinks on 01/11/2015.
+//  Copyright © 2015 Robin Spinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppyParkingTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppyParkingTest
+//
+//  Created by Robin Spinks on 01/11/2015.
+//  Copyright © 2015 Robin Spinks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
