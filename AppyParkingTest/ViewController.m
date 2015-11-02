fe249a0b25b741de078b38f681ac27a918b2fe17
Robin Spinks <uglybluecat@googlemail.com>
2015-11-02 20:09:16 +0000

    Add map view

--- AppyParkingTest/ViewController.m
+++ AppyParkingTest/ViewController.m
@@ -7,21 +7,72 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+//@import MapKit;
+
+@interface ViewController () <CLLocationManagerDelegate, MKMapViewDelegate>
+
+@property (nonatomic) MKMapView *mapView;
+@property (nonatomic) CLLocationManager  *locationManager;
+@property BOOL OKToLocate;
 
 @end
 
 @implementation ViewController
 
+#pragma mark ViewController methods
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self configureMapView];
+    [self configureLocationManager];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark custom methods
+
+- (void)configureMapView {
+    self.mapView = [[MKMapView alloc] initWithFrame:self.view.bounds];
+    [self.mapView setVisibleMapRect:MKMapRectWorld];
+    [self.view addSubview:self.mapView];
+}
+
+- (void)configureLocationManager {
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    [self.locationManager requestWhenInUseAuthorization];
+    [self updateLocationManager];
+}
+
+- (void)updateLocationManager {
+    if (self.OKToLocate) {
+        [self.locationManager startUpdatingLocation];
+        [self.mapView setShowsUserLocation:YES];
+    }
+}
+
+#pragma mark CLLocationManagerDelegate methods
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    NSLog(@"%s: %@", __PRETTY_FUNCTION__, error.debugDescription);
+}
+
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status {
+    if (status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        self.OKToLocate = YES;
+    } else {
+        self.OKToLocate = NO;
+    }
+    [self updateLocationManager];
 }
 
 @end
